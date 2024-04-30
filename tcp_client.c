9a565fe8aa7a1132142c1c7333353884a0dae5b5
Taratuhin Sergei <s_taratuhin@mail.ru>
2024-04-30 11:15:54 +0500

    Рефакторинг и создание socket.

--- tcp_client.c
+++ tcp_client.c
@@ -3,15 +3,29 @@
 #include <string.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 #define SIZE_STR 255
 #define SIZE_COMMAND 7
 
 
-static int32_t connect(char *cmd);
+typedef  struct
+{
+    uint8_t  led1 : 1;
+    uint8_t  led2 : 1;
+    uint8_t  led3 : 1;
+    uint8_t  reserv : 1;
+} LEDS;
+
+
+
+static int32_t connect_server(char *cmd);
 static int32_t parsing_connect(char *cmd, char **ip, char **port);
+static void create_socke(void);
 static int32_t parsing_command(char *cmd);
-static void create_command(char *cmd);
+static void create_command(char *cmd, LEDS *leds);
 
 
 int main(int argc, char *argv[])
@@ -25,7 +39,7 @@ int main(int argc, char *argv[])
         attempt++;
         printf("> ");
         fgets(cmd, SIZE_STR - 1, stdin);
-        if (!connect(cmd))
+        if (!connect_server(cmd))
         {
             attempt = 0;
         }
@@ -36,8 +50,10 @@ int main(int argc, char *argv[])
 }
 
 
-static int32_t connect(char *cmd)
+static int32_t connect_server(char *cmd)
 {
+    struct sockaddr_in serv_addr;
+    int32_t  sock_fd = 0;
     int32_t  retval = -10;
     char *ip = NULL;
     char *port = NULL;
@@ -51,9 +67,40 @@ static int32_t connect(char *cmd)
             printf("%s\n", ip);
             printf("%s\n", port);
             printf("# ");
-            // TODO Установление связи по TCP
+            sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+            if (sock_fd == -1)
+            {
+                printf("Socket не создался...\n");
+
+                exit(0);
+            }
+            else
+            {
+                printf("Socket успешно создан.\n");
+            }
+
+            bzero(&serv_addr, sizeof(serv_addr));
+
+            serv_addr.sin_family = AF_INET;
+            serv_addr.sin_addr.s_addr = inet_addr(ip);
+            printf("port: %d\n", atoi(port));
+            serv_addr.sin_port = htons(atoi(port));
+
+            if (connect(sock_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) != 0)
+            {
+                printf("Соединение с сервером не установлено...\n");
+
+                exit(0);
+            }
+            else
+            {
+                printf("Сеодинение с сервером установлено.\n");
+            }
+
             fgets(cmd, SIZE_STR - 1, stdin);
             parsing_command(cmd);
+            close(sock_fd);
             free(ip);
             free(port);
             retval = 0;
@@ -97,10 +144,15 @@ static int32_t parsing_connect(char *cmd, char **ip, char **port)
 }
 
 
+static void create_socke(void)
+{}
+
+
 int32_t parsing_command(char *cmd)
 {
     int32_t retval = -10;
     char *check = NULL;
+    LEDS  leds = {0, 0, 0};
 
 
     check = strtok(cmd, " ");
@@ -114,47 +166,18 @@ int32_t parsing_command(char *cmd)
     else if (!strncmp(check, "./start_leds", 12))
     {
         // TODO: отправить на сервер команду включить светодиоды
-        create_command( check );
-        /*while (check != NULL)
-        {
-            printf("1. %s\n", check);
-            if (!strcmp(check, "LED1"))
-            {
-                printf("1\n");
-            }
-            if (!strcmp(check, "LED2"))
-            {
-                printf("2\n");
-            }
-            if (!strcmp(check, "LED3"))
-            {
-                printf("3\n");
-            }
-
-            check = strtok(NULL, " \n");
-       }*/
+        create_command(check, &leds);
+        printf("%d\n", leds.led1);
+        printf("%d\n", leds.led2);
+        printf("%d\n", leds.led3);
     }
     else if (!strncmp(check, "./stop_leds", 11))
     {
         // TODO: отправить на сервер команду выключить светодиоды
-        while (check != NULL)
-        {
-            printf("1. %s\n", check);
-            if (!strcmp(check, "LED1"))
-            {
-                printf("1\n");
-            }
-            if (!strcmp(check, "LED2"))
-            {
-                printf("2\n");
-            }
-            if (!strcmp(check, "LED3"))
-            {
-                printf("3\n");
-            }
-
-            check = strtok(NULL, " \n");
-        }
+        create_command(check, &leds);
+        printf("%d\n", leds.led1);
+        printf("%d\n", leds.led2);
+        printf("%d\n", leds.led3);
     }
     else
     {
@@ -165,24 +188,23 @@ int32_t parsing_command(char *cmd)
 }
 
 
-static void create_command(char *cmd)
+static void create_command(char *cmd, LEDS *leds)
 {
     while (cmd != NULL)
     {
-        printf("1. %s\n", cmd);
         if (!strcmp(cmd, "LED1"))
         {
-            printf("1\n");
+            leds->led1 = 1;
         }
 
         if (!strcmp(cmd, "LED2"))
         {
-            printf("2\n");
+            leds->led2 = 1;
         }
 
         if (!strcmp(cmd, "LED3"))
         {
-            printf("3\n");
+            leds->led3 = 1;
         }
 
         cmd = strtok(NULL, " \n");
